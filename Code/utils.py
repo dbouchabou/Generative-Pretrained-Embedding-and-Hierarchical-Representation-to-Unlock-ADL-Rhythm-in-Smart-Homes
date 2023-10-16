ed7d8e7f07249281c9fad7c1bb9b1dba48f3be58
Damien Bouchabou <damien.bouchabou@gmail.com>
2023-10-16 11:58:55 +0200

    correct the gpt dataset loader

--- Code/utils.py
+++ Code/utils.py
@@ -2,6 +2,7 @@
 import pickle
 import json
 
+
 def load_config(config_path):
     f = open(
         config_path,
@@ -17,10 +18,11 @@ def load_train_data_gpt(data):
     train_y = None
 
     # Load the list of DataFrames from the pickle file
-    with open("datasets/" + data + "_train_data.pickle", "rb") as f:
+    with open("datasets/" + data + "_train_data_time.pickle", "rb") as f:
         train_x = pickle.load(f)
 
     return train_x, train_y
+
 
 def load_train_data_gpt_sep(data):
     train_x = None
@@ -34,6 +36,7 @@ def load_train_data_gpt_sep(data):
         train_y = pickle.load(f)
 
     return train_x, train_y
+
 
 def load_train_data_elmo(dataset_name):
     # Load train data X from the pickle file
@@ -54,7 +57,6 @@ def load_train_data_from_dataframe_time(dataset_name):
         train_x = pickle.load(f)
 
     return train_x
-
 
 
 def load_test_data_from_dataframe_time(dataset_name):
